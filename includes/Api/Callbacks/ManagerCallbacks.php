66c10fc9374b3627991a90cf8abec8f76bce51d6
amongash <amongash08@gmail.com>
2021-06-23 14:19:35 +0300

    Updated  heckbox field to show toggled state when selected

--- includes/Api/Callbacks/ManagerCallbacks.php
+++ includes/Api/Callbacks/ManagerCallbacks.php
@@ -17,7 +17,7 @@ class ManagerCallbacks extends BaseController
 
 	public function adminSectionManager()
 	{
-		echo "Manage the Sections and Features of this plugin by activating the checkboxes from the following list.";
+		echo "Manage the Sections and Features of this Plugin by activating the checkboxes from the following list.";
 	}
 
 	public function checkboxField($args)
@@ -25,18 +25,15 @@ class ManagerCallbacks extends BaseController
 		$name = $args["label_for"];
 		$classes = $args["class"];
 		$checkbox = get_option($name);
-
 		echo '<div class="' .
 			$classes .
-			'"><input type="checkbox" 
-            id="' .
+			'"><input type="checkbox" id="' .
 			$name .
-			'"
-            name="' .
+			'" name="' .
 			$name .
-			'" value="1" class="' .
+			'" value="1" class="" ' .
 			($checkbox ? "checked" : "") .
-			'"><label for="' .
+			'><label for="' .
 			$name .
 			'"><div></div></label></div>';
 	}
