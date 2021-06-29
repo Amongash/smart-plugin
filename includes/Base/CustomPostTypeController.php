a581c63e18de841c12d8bd0349f9245bf778ad90
amongash <amongash08@gmail.com>
2021-06-29 11:13:58 +0300

    Added a custom post type controller

--- includes/Base/CustomPostTypeController.php
+++ includes/Base/CustomPostTypeController.php
@@ -7,6 +7,7 @@ namespace Includes\Base;
 
 use Includes\Api\SettingsApi;
 use Includes\Base\BaseController;
+use Includes\Api\Callbacks\CptCallbacks;
 use Includes\Api\Callbacks\AdminCallbacks;
 
 class CustomPostTypeController extends BaseController
@@ -14,6 +15,7 @@ class CustomPostTypeController extends BaseController
 	public $callbacks;
 	public $subpages = [];
 	public $custom_post_types = [];
+	public $cpt_callbacks;
 
 	public function register()
 	{
@@ -25,9 +27,13 @@ class CustomPostTypeController extends BaseController
 		}
 		$this->settings = new SettingsApi();
 		$this->callbacks = new AdminCallbacks();
+		$this->cpt_callbacks = new CptCallbacks();
 		$this->setSubpages();
-		$this->settings->addSubpages($this->subpages)->register();
+		$this->setSettings();
+		$this->setSections();
+		$this->setFields();
 
+		$this->settings->addSubpages($this->subpages)->register();
 		$this->storeCustomPostTypes();
 		if (!empty($this->custom_post_types)) {
 			add_action("init", [$this, "registerCustomPostTypes"]);
@@ -36,22 +42,56 @@ class CustomPostTypeController extends BaseController
 
 	public function storeCustomPostTypes()
 	{
-		$this->custom_post_types = [
-			[
-				"post_type" => "smart_products",
-				"name" => "Products",
-				"singular_name" => "Product",
-				"public" => true,
-				"has_archive" => true,
-			],
-			[
-				"post_type" => "smart_orders",
-				"name" => "Orders",
-				"singular_name" => "Order",
-				"public" => true,
-				"has_archive" => false,
-			],
+		$option = get_option("smart_plugin_cpt");
+		// foreach ($option as $option) {
+		$this->custom_post_types[] = [
+			"post_type" => $option["post_type"],
+			"name" => $option["plural_name"],
+			"singular_name" => $option["singular_name"],
+			"menu_name" => $option["plural_name"],
+			"name_admin_bar" => $option["singular_name"],
+			"archives" => $option["singular_name"] . " Archives",
+			"attributes" => $option["singular_name"] . " Attributes",
+			"parent_item_colon" => "Parent " . $option["singular_name"],
+			"all_items" => "All " . $option["plural_name"],
+			"add_new_item" => "Add New " . $option["singular_name"],
+			"add_new" => "Add New",
+			"new_item" => "New " . $option["singular_name"],
+			"edit_item" => "Edit " . $option["singular_name"],
+			"update_item" => "Update " . $option["singular_name"],
+			"view_item" => "View " . $option["singular_name"],
+			"view_items" => "View " . $option["plural_name"],
+			"search_items" => "Search " . $option["plural_name"],
+			"not_found" => "No " . $option["singular_name"] . " Found",
+			"not_found_in_trash" =>
+				"No " . $option["singular_name"] . " Found in Trash",
+			"featured_image" => "Featured Image",
+			"set_featured_image" => "Set Featured Image",
+			"remove_featured_image" => "Remove Featured Image",
+			"use_featured_image" => "Use Featured Image",
+			"insert_into_item" => "Insert into " . $option["singular_name"],
+			"uploaded_to_this_item" => "Upload to this " . $option["singular_name"],
+			"items_list" => $option["plural_name"] . " List",
+			"items_list_navigation" => $option["plural_name"] . " List Navigation",
+			"filter_items_list" => "Filter" . $option["plural_name"] . " List",
+			"label" => $option["singular_name"],
+			"description" => $option["plural_name"] . "Custom Post Type",
+			"supports" => ["title", "editor", "thumbnail"],
+			"taxonomies" => ["category", "post_tag"],
+			"hierarchical" => false,
+			"public" => $option["public"],
+			"show_ui" => true,
+			"show_in_menu" => true,
+			"menu_position" => 5,
+			"show_in_admin_bar" => true,
+			"show_in_nav_menus" => true,
+			"can_export" => true,
+			"has_archive" => $option["has_archive"],
+			"exclude_from_search" => false,
+			"publicly_queryable" => true,
+			"capability_type" => "post",
 		];
+		// }
 	}
 
 	public function registerCustomPostTypes()
@@ -61,9 +101,48 @@ class CustomPostTypeController extends BaseController
 				"labels" => [
 					"name" => $post_type["name"],
 					"singular_name" => $post_type["singular_name"],
+					"menu_name" => $post_type["menu_name"],
+					"name_admin_bar" => $post_type["name_admin_bar"],
+					"archives" => $post_type["archives"],
+					"attributes" => $post_type["attributes"],
+					"parent_item_colon" => $post_type["parent_item_colon"],
+					"all_items" => $post_type["all_items"],
+					"add_new_item" => $post_type["add_new_item"],
+					"add_new" => $post_type["add_new"],
+					"new_item" => $post_type["new_item"],
+					"edit_item" => $post_type["edit_item"],
+					"update_item" => $post_type["update_item"],
+					"view_item" => $post_type["view_item"],
+					"view_items" => $post_type["view_items"],
+					"search_items" => $post_type["search_items"],
+					"not_found" => $post_type["not_found"],
+					"not_found_in_trash" => $post_type["not_found_in_trash"],
+					"featured_image" => $post_type["featured_image"],
+					"set_featured_image" => $post_type["set_featured_image"],
+					"remove_featured_image" => $post_type["remove_featured_image"],
+					"use_featured_image" => $post_type["use_featured_image"],
+					"insert_into_item" => $post_type["insert_into_item"],
+					"uploaded_to_this_item" => $post_type["uploaded_to_this_item"],
+					"items_list" => $post_type["items_list"],
+					"items_list_navigation" => $post_type["items_list_navigation"],
+					"filter_items_list" => $post_type["filter_items_list"],
 				],
+				"label" => $post_type["label"],
+				"description" => $post_type["description"],
+				"supports" => $post_type["supports"],
+				"taxonomies" => $post_type["taxonomies"],
+				"hierarchical" => $post_type["hierarchical"],
 				"public" => $post_type["public"],
+				"show_ui" => $post_type["show_ui"],
+				"show_in_menu" => $post_type["show_in_menu"],
+				"menu_position" => $post_type["menu_position"],
+				"show_in_admin_bar" => $post_type["show_in_admin_bar"],
+				"show_in_nav_menus" => $post_type["show_in_nav_menus"],
+				"can_export" => $post_type["can_export"],
 				"has_archive" => $post_type["has_archive"],
+				"exclude_from_search" => $post_type["exclude_from_search"],
+				"publicly_queryable" => $post_type["publicly_queryable"],
+				"capability_type" => $post_type["capability_type"],
 			]);
 		}
 	}
@@ -80,5 +159,97 @@ class CustomPostTypeController extends BaseController
 				"callback" => [$this->callbacks, "adminCpt"],
 			],
 		];
+	}
+
+	public function setSettings()
+	{
+		$args[] = [
+			"option_group" => "smart_plugin_cpt_settings",
+			"option_name" => "smart_plugin_cpt",
+			"callback" => [$this->cpt_callbacks, "cptSanitize"],
+		];
+
+		$this->settings->setSettings($args);
+	}
+
+	public function setSections()
+	{
+		$args = [
+			[
+				"id" => "smart_cpt_index",
+				"title" => "Custom Post Type Manager",
+				"callback" => [$this->cpt_callbacks, "cptSectionManager"],
+				"page" => "smart_plugin_cpt",
+			],
+		];
+		$this->settings->setSections($args);
+	}
+
+	public function setFields()
+	{
+		$args = [
+			[
+				"id" => "post_type",
+				"title" => "Custom Post Type ID",
+				"callback" => [$this->cpt_callbacks, "textField"],
+				"page" => "smart_plugin_cpt",
+				"section" => "smart_cpt_index",
+				"args" => [
+					"option_name" => "smart_plugin_cpt",
+					"label_for" => "post_type",
+					"placeholder" => "eg. product",
+				],
+			],
+			[
+				"id" => "singular_name",
+				"title" => "Singular Name",
+				"callback" => [$this->cpt_callbacks, "textField"],
+				"page" => "smart_plugin_cpt",
+				"section" => "smart_cpt_index",
+				"args" => [
+					"option_name" => "smart_plugin_cpt",
+					"label_for" => "singular_name",
+					"placeholder" => "eg. Product",
+				],
+			],
+			[
+				"id" => "plural_name",
+				"title" => "Plural Name",
+				"callback" => [$this->cpt_callbacks, "textField"],
+				"page" => "smart_plugin_cpt",
+				"section" => "smart_cpt_index",
+				"args" => [
+					"option_name" => "smart_plugin_cpt",
+					"label_for" => "plural_name",
+					"placeholder" => "eg. Products",
+				],
+			],
+			[
+				"id" => "public",
+				"title" => "Public",
+				"callback" => [$this->cpt_callbacks, "checkboxField"],
+				"page" => "smart_plugin_cpt",
+				"section" => "smart_cpt_index",
+				"args" => [
+					"option_name" => "smart_plugin_cpt",
+					"label_for" => "public",
+					"class" => "ui-toggle",
+				],
+			],
+			[
+				"id" => "has_archive",
+				"title" => "Archive",
+				"callback" => [$this->cpt_callbacks, "checkboxField"],
+				"page" => "smart_plugin_cpt",
+				"section" => "smart_cpt_index",
+				"args" => [
+					"option_name" => "smart_plugin_cpt",
+					"label_for" => "has_archive",
+					"class" => "ui-toggle",
+				],
+			],
+		];
+
+		$this->settings->setFields($args);
 	}
 }
