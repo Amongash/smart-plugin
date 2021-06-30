ba83def6b3984cdc533eccc519c0a6ccbb69a26f
amongash <amongash08@gmail.com>
2021-06-30 17:06:45 +0300

    Modified CPT manager

--- includes/Api/Callbacks/CptCallbacks.php
+++ includes/Api/Callbacks/CptCallbacks.php
@@ -16,6 +16,11 @@ class CptCallbacks
 	{
 		$output = get_option("smart_plugin_cpt");
 
+		if (isset($_POST["remove"])) {
+			unset($output[$_POST["remove"]]);
+			return $output;
+		}
+
 		if (count($output) == 0) {
 			$output[$input["post_type"]] = $input;
 			return $output;
@@ -36,8 +41,12 @@ class CptCallbacks
 	{
 		$name = $args["label_for"];
 		$option_name = $args["option_name"];
-		$input = get_option($option_name);
-		// $value = $input[$name];
+		$value = "";
+
+		if (isset($_POST["edit_post"])) {
+			$input = get_option($option_name);
+			$value = $input[$_POST["edit_post"]][$name];
+		}
 
 		echo '<input type="text" class="regular-text" id="' .
 			$name .
@@ -45,21 +54,24 @@ class CptCallbacks
 			$option_name .
 			"[" .
 			$name .
-			']" value="" placeholder="' .
+			']" value="' .
+			$value .
+			'" placeholder="' .
 			$args["placeholder"] .
 			'" required>';
 	}
+
 	public function checkboxField($args)
 	{
 		$name = $args["label_for"];
 		$classes = $args["class"];
 		$option_name = $args["option_name"];
-		$checkbox = get_option($option_name);
-		$checked = isset($checkbox[$name])
-			? ($checkbox[$name]
-				? true
-				: false)
-			: false;
+		$checked = false;
+
+		if (isset($_POST["edit_post"])) {
+			$checkbox = get_option($option_name);
+			$checked = isset($checkbox[$_POST["edit_post"]][$name]) ?: false;
+		}
 
 		echo '<div class="' .
 			$classes .
@@ -69,7 +81,9 @@ class CptCallbacks
 			$option_name .
 			"[" .
 			$name .
-			']" value="1" class="" ><label for="' .
+			']" value="1" class="" ' .
+			($checked ? "checked" : "") .
+			'><label for="' .
 			$name .
 			'"><div></div></label></div>';
 	}
--- templates/cpt.php
+++ templates/cpt.php
@@ -4,32 +4,59 @@
 
     
     <ul class=" nav nav-tabs">
-        <li class="active"><a href="#tab-1">Your Custom Post Types</a></li>
-        <li><a href="#tab-2">Add Custom Post Types</a></li>
-        <li><a href="#tab-3">Export</a></li>
+    <li class="<?php echo !isset($_POST["edit_post"])
+    	? "active"
+    	: ""; ?>"><a href="#tab-1">Your Custom Post Types</a></li>
+		<li class="<?php echo isset($_POST["edit_post"]) ? "active" : ""; ?>">
+			<a href="#tab-2">
+				<?php echo isset($_POST["edit_post"]) ? "Edit" : "Add"; ?> Custom Post Type
+			</a>
+		</li>
+		<li><a href="#tab-3">Export</a></li>
     </ul>
 
     <div class="tab-content">
-        <div id="tab-1" class="tab-pane active">
+        <div id="tab-1" class="tab-pane  <?php echo !isset($_POST["edit_post"])
+        	? "active"
+        	: ""; ?>">
         <h3>Manage Your Custom Post Types</h3>
 
         <?php
-        // $options = !get_option("smart_plugin_cpt")
-        // 	? []
-        // 	: get_option("smart_plugin_cpt");
         $options = get_option("smart_plugin_cpt") ?: [];
         echo '<table class="cpt-table"><tr><th>ID</th><th>Singular Name</th><th>Plural Name</th><th class="text-center">Public</th><th class="text-center">Archive</th><th class="text-center">Actions</th></tr>';
 
         foreach ($options as $option) {
-        	$public = isset($options["public"]) ? "TRUE" : "FALSE";
-        	$archive = isset($options["has_archive"]) ? "TRUE" : "FALSE";
-        	echo "<tr><td>{$option["post_type"]}</td><td>{$option["singular_name"]}</td><td>{$option["plural_name"]}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\"><a href=\"#\">EDIT</a> - <a href=\"#\">DELETE</a></td></tr>";
+        	$public = isset($option["public"]) ? "TRUE" : "FALSE";
+        	$archive = isset($option["has_archive"]) ? "TRUE" : "FALSE";
+        	echo "<tr><td>{$option["post_type"]}</td><td>{$option["singular_name"]}</td><td>{$option["plural_name"]}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\">";
+
+        	echo '<form method="post" action="" class="inline-block">';
+        	echo '<input type="hidden" name="edit_post" value="' .
+        		$option["post_type"] .
+        		'">';
+        	submit_button("Edit", "primary small", "submit", false);
+        	echo "</form> ";
+
+        	echo '<form method="post" action="options.php" class="inline-block">';
+        	settings_fields("smart_plugin_cpt_settings");
+        	echo '<input type="hidden" name="remove" value="' .
+        		$option["post_type"] .
+        		'">';
+        	submit_button("Delete", "delete small", "submit", false, [
+        		"onclick" =>
+        			'return confirm("Are you sure you want to delete this Custom Post Type? The data associated with it will not be deleted.");',
+        	]);
+
+        	echo "</form>";
+        	echo "</td></tr>";
         }
 
         echo "</table>";
         ?>
         </div>
-        <div id="tab-2" class="tab-pane">
+        <div id="tab-2" class="tab-pane  <?php echo isset($_POST["edit_post"])
+        	? "active"
+        	: ""; ?>">
         
             <form method="post" action="options.php">
             <?php
@@ -39,6 +66,6 @@
             ?>
             </form>
         </div>
-        <div id="tab-3" class="tab-pane"><h3>Export Custom Post Types</a>h3></div>
+        <div id="tab-3" class="tab-pane "><h3>Export Custom Post Types</a><h3></div>
     </div>
 </div>  
