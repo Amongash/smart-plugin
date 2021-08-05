e02f3527a1160dfdc4b724038c2b4ba3d60a15ad
amongash <amongash08@gmail.com>
2021-08-05 13:43:44 +0300

    Comment out unused modules

--- includes/Base/BaseController.php
+++ includes/Base/BaseController.php
@@ -23,12 +23,13 @@ class BaseController
 			"taxonomy_manager" => "Activate Taxonomy Manager",
 			// "media_widget" => "Activate Media Widget",
 			// "gallery_manager" => "Activate Gallery Manager",
-			"testimonial_manager" => "Activate Testimonial Manager",
+			// "testimonial_manager" => "Activate Testimonial Manager",
 			"templates_manager" => "Activate Custom Templates",
-			"login_manager" => "Activate Ajax Login/Signup",
-			"membership_manager" => "Activate Membership Manager",
-			"chat_manager" => "Activate Chat Manager",
+			// "login_manager" => "Activate Ajax Login/Signup",
+			// "membership_manager" => "Activate Membership Manager",
+			// "chat_manager" => "Activate Chat Manager",
 			"contact_form" => "Activate Contact Form",
+			"register_form" => "Activate Registration Section"
 		];
 	}
 
